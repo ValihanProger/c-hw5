a0e93a8bf768cd66299683bc86c23aeaad0dda1b
Valihan <valihanburhanov@yandex.ru>
2023-01-29 10:54:23 +0300

    task3 complete

--- c#hw5.cs
+++ c#hw5.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+int b = new Random().Next(3, 9);
+int[] numbers = new int[b];
+Console.Write("[ ");
+for (int i = 0; i < numbers.Length; i++)
+{
+    int a = new Random().Next(100,1000);
+    numbers[i] = a;
+    Console.Write($"{numbers[i]} ");
+}
+
+int min = numbers.Min();
+int max = numbers.Max();
+
+Console.Write($"] -> {max-min}");
