03535b36547eed3f09c4d4f10510ef07b94b552e
Valihan <valihanburhanov@yandex.ru>
2023-01-29 10:48:02 +0300

    task2 complete

--- c#hw5.cs
+++ c#hw5.cs
@@ -3,5 +3,22 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-
+int b = new Random().Next(3, 9);
+int[] numbers = new int[b];
+int ans = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    int a = new Random().Next(100,1000);
+    numbers[i] = a;
+    Console.Write($"{numbers[i]}, ");
+    if (numbers[i] % 2 != 0)
+    {
+        ans = ans + numbers[i];
+    }
+    else
+    {
+        continue;
+    } 
+}
+Console.WriteLine($"Сумма нечетных чисел = {ans}"); 
 
